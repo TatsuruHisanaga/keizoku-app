0cd894d9bf8545707be2b34b7a38920ff9d6f565
Tatsuru Hisanaga <tatsu72x@gmail.com>
2025-01-15 13:58:50 +0900

    fix: babel.config.jsのプラグイン設定を整理し、global.cssの重複を削除
    以下のエラーを解消
    ERROR node_modules/expo-router/entry.js: Duplicate plugin/preset detected.

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,22 @@
 module.exports = function (api) {
   api.cache(true);
-  api.cache(true);
+
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
-      "nativewind/babel"
     ],
-
-    plugins: [["module-resolver", {
-      root: ["./"],
-
-      alias: {
-        "@": "./",
-        "tailwind.config": "./tailwind.config.js"
-      }
-    }]]
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './',
+            'tailwind.config': './tailwind.config.js',
+          },
+        },
+      ],
+    ],
   };
 };
