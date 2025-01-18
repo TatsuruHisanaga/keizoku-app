03a5dde9518255bb343cb4f517cd908d0e0c0825
Tatsuru Hisanaga <tatsu72x@gmail.com>
2025-01-18 13:50:23 +0900

    refactor: 不要なインポートを消去

--- components/HabitItem.tsx
+++ components/HabitItem.tsx
@@ -3,8 +3,6 @@ import { Button, ButtonIcon } from '@/components/ui/button'
 import { Heading } from './ui/heading'
 import { Box } from './ui/box'
 import { Text } from './ui/text'
-import { Audio } from 'expo-av'
-import { useEffect } from 'react'
 
 export interface HabitItemProps {
   habit: {
