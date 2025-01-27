ec302c78be1809543a1d69e961f0e6d8a2051dcb
Tatsuru Hisanaga <tatsu72x@gmail.com>
2025-01-27 19:56:48 +0900

    refactor: Prettierのエラーを解消

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout() {
     <GluestackUIProvider>
       <GestureHandlerRootView style={{ flex: 1 }}>
         <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         </Stack>
       </GestureHandlerRootView>
     </GluestackUIProvider>
