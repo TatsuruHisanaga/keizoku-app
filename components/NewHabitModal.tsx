49205bc494b4590e4eeed7e4cb9a94b3d3a09282
uonoe <hina.1018.hina@gmail.com>
2025-02-01 16:54:06 +0900

    feat: 目標設定機能を追加

--- components/NewHabitModal.tsx
+++ components/NewHabitModal.tsx
@@ -12,7 +12,7 @@ import { Button, ButtonText } from '@/components/ui/button';
 import { Box } from '@/components/ui/box';
 import { Center } from '@/components/ui/center';
 import { PartyPopper } from 'lucide-react-native';
-import { useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import LottieView from 'lottie-react-native';
 
 interface NewHabitModalProps {
@@ -27,6 +27,12 @@ export default function NewHabitModal({
   habitName,
 }: NewHabitModalProps) {
   const confettiRef = useRef<LottieView | null>(null);
+  const [goal, setGoal] = useState(false);
+
+  const handleClose = () => {
+    setGoal(false);
+    onClose();
+  }
 
   useEffect(() => {
     if (isOpen) {
@@ -73,37 +79,72 @@ export default function NewHabitModal({
 
   return (
     <Center>
-      <Modal isOpen={isOpen} onClose={onClose} size="sm" style={{ zIndex: 10 }}>
+      <Modal isOpen={isOpen} onClose={handleClose} size="sm" style={{ zIndex: 10 }}>
         <ModalBackdrop />
         <ModalContent>
-          <Box className="w-20 h-20 bg-green-100 rounded-full flex items-center justify-center mx-auto mb-4">
-            <PartyPopper color="#22c55e" />
-          </Box>
-          <ModalHeader>
-            <Box></Box>
-            <Heading size="md" className="text-typography-950 mb-2">
-              新しい習慣を追加しました！
-            </Heading>
-            <Box></Box>
-          </ModalHeader>
-          <ModalBody>
-            <Text size="sm" className="text-typography-500 text-center">
-              継続は力なり！頑張りましょう！
-            </Text>
-          </ModalBody>
-          <ModalFooter>
-            <Box
-              style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}
-            >
-              <Button onPress={onClose} className="w-full">
-                <ButtonText>始める</ButtonText>
-              </Button>
+          {!goal ? (
+            <>
+            <Box className="w-20 h-20 bg-green-100 rounded-full flex items-center justify-center mx-auto mb-4">
+              <PartyPopper color="#22c55e" />
             </Box>
+            <ModalHeader>
+              <Box></Box>
+              <Heading size="md" className="text-typography-950 mb-2">
+                新しい習慣を追加しました！
+              </Heading>
+              <Box></Box>
+            </ModalHeader>
+            <ModalBody>
+              <Text size="sm" className="text-typography-500 text-center">
+                継続は力なり！頑張りましょう！
+              </Text>
+            </ModalBody>
+            <ModalFooter>
+              <Box
+                style={{
+                  flex: 1,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <Button onPress={() => setGoal(true)} className="mb-1 w-full">
+                  <ButtonText>目標を設定する</ButtonText>
+                </Button>
+                <Button onPress={handleClose} className="w-full">
+                  <ButtonText>始める</ButtonText>
+                </Button>
+              </Box>
           </ModalFooter>
+          </>
+          ) : (
+            <>
+            <ModalHeader>
+              <Box></Box>
+              <Heading size="md" className="text-typography-950 mb-2">
+                目標を設定しましょう！
+              </Heading>
+              <Box></Box>
+            </ModalHeader>
+            <ModalBody>
+              <Text size="sm" className="text-typography-500 text-center">
+                今日から始める習慣の目標を設定しましょう！
+              </Text>
+            </ModalBody>
+            <ModalFooter>
+              <Box
+                style={{
+                  flex: 1,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <Button onPress={handleClose} className="w-full">
+                  <ButtonText>始める</ButtonText>
+                </Button>
+              </Box>
+            </ModalFooter>
+            </>
+          )}
         </ModalContent>
         {renderLottieView()}
       </Modal>
